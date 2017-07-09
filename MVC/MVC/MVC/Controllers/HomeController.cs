c22df83e099fc0cd8f664990046d0a34abc74465
DESKTOP-64J0TUJ\WAVELEE <1322720362@qq.com>
2017-07-10 03:04:52 +0800

    no message

---
+++ MVC/MVC/MVC/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Document()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Task()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult EditNews()
+        {
+            ViewBag.Message = "Your page.";
+
+            return View();
+        }
+    }
+}
---
+++ MVC/MVC/MVC/Scripts/Layout.js
@@ -0,0 +1,20 @@
+﻿var change = window.location.href;
+var oNav = document.getElementById('navn');
+var oli = oNav.getElementsByTagName('li');
+for (let i = 0; i < oli.length - 1; i++) {
+    if (oli[i].className == 'active') {
+        oli[i].className = '';
+    }
+}
+if (change.includes('Home/Task')) {
+    oli[5].className = 'active';
+}
+else if (change.includes('Home/Document')) {
+    oli[4].className = 'active';
+}
+else if (change.includes('Home/Feedback')) {
+    oli[3].className = 'active';
+}
+else {
+    oli[2].className = 'active';
+}
