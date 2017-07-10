d38b405044c906c069caa769c493c7f077cdd7af
DESKTOP-M40RQA1\Wavelee <1322720362@qq.com>
2017-07-10 22:32:25 +0800

    Front-AdminUI

---
+++ MVC/MVC/Scripts/EditNews.js
@@ -0,0 +1,25 @@
+﻿window.onload = function () {
+    oSave = document.getElementById('save');
+    oDept = document.getElementsByClassName('department');
+    oSelect = document.getElementById('selectperson');
+    oDate = document.getElementById('published_data');
+    var date = new Date();
+    //oDate.innerHTML = date.getFullYear() + '/' + (date.getMonth() + 1) + '/' + date.getDate();
+    //console.log(11111);
+    oSave.addEventListener('click', function () {
+        //console.log(11111);
+        var name = '';
+        for (var i = 0; i < oDept.length; i++) {
+            //console.log(oDept[i].childNodes);
+            console.log(11111);
+            for (var j = 1; j < oDept[i].childNodes.length; j = j + 2) {
+                if (oDept[i].childNodes[j].childNodes[1].childNodes[1].checked) {
+                    var name=name+oDept[i].childNodes[j].childNodes[1].childNodes[7].innerHTML+'; ';
+                }
+                
+                //oDept[i].childNodes[(2 * j + 1)].childNodes[1].childNodes[1];
+            }
+        }
+        oSelect.innerHTML = name;
+    });
+}
---
+++ MVC/MVC/Scripts/News.js
@@ -0,0 +1,53 @@
+﻿window.onload = function () {
+    var all = document.getElementById('all');
+    var boxs = document.getElementsByClassName('chbox');
+    all.onclick = function () {
+        if (all.checked == true) {
+            for (var i = 0; i < boxs.length; i++) {
+                boxs[i].checked = true;
+            }
+        }
+        else {
+            for (var i = 0; i < boxs.length; i++) {
+                boxs[i].checked = false;
+            }
+        }
+    }
+    var oUl = document.getElementsByClassName('linkbutton')[0];
+    var oLi = oUl.getElementsByTagName('li');
+    for (let i = 0; i < oLi.length; i++) {
+        if (i == 0) {
+            oLi[i].addEventListener('onclick', function () {
+                var temp = null;
+                for (let j = 1; j < oLi.length - 1; j++) {
+                    if (oLi[j].className == 'active') {
+                        temp = j;
+                        oLi[j].className = '';
+                    }
+                }
+                if (temp != 1) {
+                    oLi[temp - 1].className = 'active';
+                }
+                else if (parseInt(oLi[temp].innerHTML) != 1) {
+                    for (let j = 1; j < oLi.length - 1; j++) {
+                        oLi[j].innerHTML = parseInt(oLi[j].innerHTML) - 1;
+                    }
+                }
+            })
+        }
+        else if (i == (oLi.length - 1)) {
+
+        }
+        else {
+            //console.log(i);
+            oLi[i].addEventListener('click', function () {
+                for (let j = 1; j < oLi.length - 1; j++) {
+                    if (oLi[j].className == 'active') {
+                        oLi[j].className = '';
+                    }
+                }
+                this.className = 'active';
+            })
+        }
+    }
+}
